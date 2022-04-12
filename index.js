88b38a3fa7ded3e5ccc7608654d973fabdbb0802
Taylor25et <taylor25et@gmail.com>
2022-04-11 18:59:25 -0700

    added start menu to command line

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const inquirer = require('inquirer');
-const db = require('./db/connection');
-require('console.table');
+const inquirer = require("inquirer");
+const db = require("./db/connection");
+require("console.table");
+
+const {
+  startPrompts,
+  addEmployee,
+  addDepartment,
+  addRole,
+  updateRole,
+} = require("./prompts/prompts");
+
+const startMenu = () => {
+  inquirer.prompt(startPrompts).then((response) => {
+    switch (response.options) {
+      case "Add Employee":
+        newEmployee();
+        break;
+      case "Add Department":
+        newDepartment();
+        break;
+      case "Add Role":
+        newRole();
+        break;
+      case "View Employees":
+        viewEmployees();
+        break;
+      case "View Departments":
+        viewDepartments();
+        break;
+      case "Update Employee Role":
+        changeRole();
+        break;
+      case "Quit":
+        console.log("Goodbye");
+        db.end();
+    }
+  });
+};
